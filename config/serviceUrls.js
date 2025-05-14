fb021a6c39fcad7715e9a244229db5fe3441b26b
luis-pedro-dutra-carrocini <luispedrodutracarrocini58@gmail.com>
2025-05-14 00:55:33 -0300

    Teste 2

--- config/serviceUrls.js
+++ config/serviceUrls.js
@@ -3,10 +3,10 @@
 // Estas são URLs de exemplo. Substitua pelas URLs reais onde seus microserviços estão hospedados.
 
 const SERVICE_URLS = {
-  add: process.env.ADD_SERVICE_URL || 'https://soma-psi.vercel.app/api',
+  add: process.env.ADD_SERVICE_URL || 'https://soma-psi.vercel.app/',
   subtract: process.env.SUBTRACT_SERVICE_URL || 'http://localhost:3002/subtract',
   multiply: process.env.MULTIPLY_SERVICE_URL || 'http://localhost:3003/multiply',
-  divide: process.env.DIVIDE_SERVICE_URL || 'https://calculadora-microservicos.vercel.app/divide'
+  divide: process.env.DIVIDE_SERVICE_URL || 'https://calculadora-microservi-git-960650-luis-pedros-projects-1815b3dd.vercel.app/divide'
 };
 
 export default SERVICE_URLS;
