e19669437db35776464195cef9ea2dd7c72c0138
luis-pedro-dutra-carrocini <luispedrodutracarrocini58@gmail.com>
2025-05-15 21:30:08 -0300

    Add

--- api/soma.js
+++ api/soma.js
@@ -1,5 +1,8 @@
 export default function handler(req, res) {
-  const { a, b } = req.query;
+  const { a, b } = req.params;
+  if (isNaN(a) || isNaN(b)) {
+    return res.status(400).json({ error: 'Parâmetros inválidos' });
+  }
   const result = Number(a) + Number(b);
   res.status(200).json({ result });
 }
